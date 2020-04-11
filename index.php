03b5b6ee7e03195bfb64783e2386ea7e73a33754
ryanmuka <37466116+ryanmuka@users.noreply.github.com>
2020-04-11 18:56:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,83 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-define("OS", strtolower(PHP_OS));
-header('Content-Type: text/html; charset=utf-8');
-error_reporting(0);
-
+﻿<?php
+set_time_limit(0);
+include 'download.php';
+clear();
 echo "
-Ξ TITLE  : Toolsig PHP Version [Beta Release]
-Ξ UPLOAD : c:/users/officialputuid/toolsig-php
-Ξ CODEBY : Firdy
-Ξ FIXBY	 : Andi Muh. Rizqi [ikiganteng]
-Ξ UPDATE : officialputuid [25/01/2020]
-Ξ Recode : CJDW Yans [25/01/2020]
-•••••••••••••••••••••••••••••••••••••••••
-1. FLC Target Followers (fft)
-2. Auto Like Timeline (botlike)
-3. Unfollow All Following
-4. Unfollow Not Followback
-•••••••••••••••••••••••••••••••••••••••••
+ *  INSTAGRAM FEED LIKER
+ *  RECODE #CJDW Team
+ 
+  
+    •••••••••••••••••••••••••••••••••••••••••
+    
+ * Use tools at your own risk.
+ * Use this Tool for personal use, not for sale.
+ * I am not responsible for your account using this tool.
+ * Make sure your account has been verified (Email & Telp).
+ 
 ";
+$username    = getUsername();
+$password    = getPassword();
 
-echo "[?] Enter Your Choice (1-4): ";
+echo '•••••••••••••••••••••••••••••••••••••••••' . PHP_EOL . PHP_EOL;
+$login = login($username, $password);
+if ($login['status'] == 'success') {
+    echo '[*] Login as ' . $login['username'] . ' Success!' . PHP_EOL;
+    $data_login = array(
+        'username' => $login['username'],
+        'csrftoken' => $login['csrftoken'],
+        'sessionid' => $login['sessionid']
+    );
 
-$input=fgets(STDIN);
-switch ($input) {
-	case 0:
-        require('index.php');
-        break;
-	case 1:
-        require('tools/fftauto.php');
-        break;
-    case 2:
-        require('tools/botlike_timeline.php');
-        break;
-    case 3:
-        require('tools/unfollall.php');
-        break;
-    case 4:
-        require('tools/unfollnotfollback.php');
-        break;
-}
-?>
+    $slee = getComment('[?]  Sleep in Seconds ( RECOMMENDED 100 ) : ');
+  
+    while (true) {
+
+        if (n8off() == true):
+       $sleep = $slee + rand(0,10);
+        $profile    = getHome($data_login);
+        $data_array = json_decode($profile);
+        $result     = $data_array->user->edge_web_feed_timeline;
+        foreach ($result->edges as $items) {
+            $id       = $items->node->id;
+            $username = $items->node->owner->username;
+            $like     = like($id, $data_login);
+            if ($like['status'] == 'error') {
+                echo '[+] Username: ' . $username . ' | Media_id: ' . $id . ' | Error Like' . PHP_EOL;
+                logout($data_login);
+                $login = login($username, $password);
+                if ($login['status'] == 'success') {
+                    echo '[*] Login as ' . $login['username'] . ' Success!' . PHP_EOL;
+                    $data_login = array(
+                        'username' => $login['username'],
+                        'csrftoken' => $login['csrftoken'],
+                        'sessionid' => $login['sessionid']
+                    );
+                }else{
+
+                    die("Something went wrong");
+
+                }
+            } else {
+                echo '[+] Username: ' . $username . ' | Media_id: ' . $id . ' | Like Success' . PHP_EOL;
+            }
+            break;
+        }
+        echo '[+] [' . date("H:i:s") . '] Sleep for ' . $sleep . ' seconds [+]' . PHP_EOL;
+        sleep( $sleep);
+        echo '•••••••••••••••••••••••••••••••••••••••••' . PHP_EOL . PHP_EOL;
+        else:
+
+            echo "Next Run time is 11 AM".PHP_EOL.PHP_EOL;
+
+        sleep(rand(100,600));
+
+        endif;
+
+    }
+
+
+
+
+} else
+
+    echo json_encode($login);
